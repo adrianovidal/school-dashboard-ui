797df7403ce5ba92e167f72fc90afd686ff039b3
adriano_vidal <adriano_vidal@sicredi.com.br>
2025-08-08 23:34:33 +0100

    Novos components, Anuncios e Eventos

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,8 @@
 
+import Announcements from "@/components/Announcements"
 import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
+import EventCalendar from "@/components/EventCalendar"
 import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
@@ -26,7 +28,10 @@ const AdminPage = () => {
                     <FinanceChart />
                 </div>
             </div>
-            <div className="w-full lg:w-1/3"></div>
+            <div className="w-full lg:w-1/3 flex flex-col gap-8">
+                <EventCalendar />
+                <Announcements />
+            </div>
         </div>
     )
 }
