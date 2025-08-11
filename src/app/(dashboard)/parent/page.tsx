f97b752ca425f14dfa9f98d8b5f096654bc7d6a9
adriano_vidal <adriano_vidal@sicredi.com.br>
2025-08-11 23:21:38 +0100

    Lista de professores

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,6 +1,19 @@
+import Announcements from "@/components/Announcements"
+import BigCalendar from "@/components/BigCalendar"
+
 const ParentPage = () => {
     return (
-        <div className="">ParentPage</div>
+        <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+            <div className="w-full xl:w-2/3">
+                <div className="h-full bg-white p-4 rounded-md">
+                    <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+                    <BigCalendar />
+                </div>
+            </div>
+            <div className="w-full lg:w-1/3">
+                <Announcements />
+            </div>
+        </div>
     )
 }
 
